546630c3357023bbf3053c6faa48bf6848c0b52c
RaymarMonte <raymarmontecs@gmail.com>
2025-10-27 20:56:27 +0800

    Add initial Spring Initializer files

---
+++ src/main/java/com/raymar/RepLog/RepLogApplication.java
@@ -0,0 +1,13 @@
+package com.raymar.RepLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepLogApplication.class, args);
+	}
+
+}
